09474fc357ab48f38eb68089eef10ec511611692
scrubi19 <scrubi.memo@yandex.ru>
2020-12-04 17:40:43 +0700

    feature min path from vertices

--- src/main/java/DynamicMinPathInGraph/DynamicMinPathSearch.java
+++ src/main/java/DynamicMinPathInGraph/DynamicMinPathSearch.java
@@ -1,25 +1,47 @@
 package DynamicMinPathInGraph;
 
-public class DynamicMinPath {
-    Graph graph;
+import static DynamicMinPathInGraph.Graph.INF;
 
-    public DynamicMinPath(Graph graph) {
-        this.graph = graph;
-    }
+public class DynamicMinPathSearch {
 
-    public void floydWarshallCalculate(int[][] matrix, int numberOfVert) {
-        //Пробегаемся по всем вершинам и ищем более короткий путь
-        //через вершину k
-        for(int k = 0; k < numberOfVert; k++) {
-            for(int i = 0; i < numberOfVert; i++) {
-                for(int j = 0; j < numberOfVert; j++) {
-                    //Новое значение ребра равно минимальному между старым
-                    //и суммой ребер i <-> k + k <-> j (если через k пройти быстрее)
-                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+    public static int[][] floydWarshallCalculate(int[][] matrix, int size) {
+        int[][] parent = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (matrix[i][j] != 0 && matrix[i][j] != INF) {
+                    parent[i][j] = i;
+
+                }
+
+            }
+        }
+        Graph.printGraph("Parent", parent);
+
+        for(int k = 0; k < size; k++) {
+            for(int i = 0; i < size; i++) {
+                for(int j = 0; j < size; j++) {
+                    if (matrix[i][k] < INF && matrix[k][j] < INF
+                            && matrix[i][k] + matrix[k][j] < matrix[i][j]) {
+//                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                        matrix[i][j] = matrix[i][k] + matrix[k][j];
+                        parent[i][j] = parent[k][j];
+                    }
                 }
             }
         }
+        Graph.printGraph("Parent after for", parent);
 
-        return;
+
+        return parent;
     }
+
+    public static void getShortestPath(int[][] matrix, int u,  int v) {
+        int c = u;
+        while (c != v) {
+            System.out.print(c+" ");
+            c = matrix[c][v];
+        }
+        System.out.print(v+" ");
+    }
+
 }
--- src/main/java/DynamicMinPathInGraph/Main.java
+++ src/main/java/DynamicMinPathInGraph/Main.java
@@ -1,2 +1,18 @@
-package DynamicMinPathInGraph;public class Main {
+package DynamicMinPathInGraph;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.getGraphFromFile();
+        Graph.printGraph("Graph", graph.matrix_G);
+
+        int[][]parent =  DynamicMinPathSearch.floydWarshallCalculate(graph.matrix_G, graph.size);
+
+        Graph.printGraph("Graph after Floyd Warshall Method", graph.matrix_G);
+
+        DynamicMinPathSearch.getShortestPath(parent, 1, 5);
+
+
+    }
 }
